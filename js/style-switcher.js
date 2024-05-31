ca417511adf6d93f06244a5b8a58049155e02821
SivaError <sivas554781@gmail.com>
2024-05-31 14:21:57 +0530

    protfolio

---
+++ js/style-switcher.js
@@ -0,0 +1,9 @@
+/*toggle style switcher */
+document.addEventListener("DOMContentLoaded", () => {
+    const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+    styleSwitcherToggle.addEventListener("click", () => {
+        document.querySelector(".style-switcher").classList.toggle("open");
+    });
+});
+
+
